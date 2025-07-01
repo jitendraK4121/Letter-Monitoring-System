784e5563a00d308ec771a7f60b4955917684b571
jitendraK4121 <jitendrakumarsahoo201919@gmail.com>
2025-07-01 23:20:31 +0530

    fix: Update auth routes to use correct middleware and controller references

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, changePassword, changeUserPassword, initializeUsers } = require('../controllers/auth.controller');
-const { protect } = require('../middleware/auth');
+const authController = require('../controllers/auth.controller');
+const auth = require('../middleware/auth');
 
-router.post('/register', register);
-router.post('/login', login);
-router.post('/change-password', protect, changePassword);
-router.post('/change-user-password', protect, changeUserPassword);
-router.get('/users', protect, getAllUsers);
-router.post('/users', protect, createUser);
-router.post('/init-users', initializeUsers);
+// Public routes
+router.post('/login', authController.login);
+router.post('/init-users', authController.initializeUsers);
+
+// Protected routes
+router.post('/change-password', auth, authController.changePassword);
+router.post('/change-user-password', auth, authController.changeUserPassword);
+router.get('/users', auth, authController.getAllUsers);
+router.post('/users', auth, authController.createUser);
 
 module.exports = router; 
