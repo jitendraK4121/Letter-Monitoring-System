23df334fd5404b0927f3e865a2da695400266f48
jitendraK4121 <jitendrakumarsahoo201919@gmail.com>
2025-07-01 22:34:57 +0530

    fix: Update login API endpoint to use config URL

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -6,6 +6,7 @@ import {
   Button,
   styled
 } from '@mui/material';
+import config from '../../config';
 
 const LoginContainer = styled(Box)({
   minHeight: '100vh',
@@ -83,7 +84,7 @@ const Login = () => {
     e.preventDefault();
     
     try {
-      const response = await fetch('http://localhost:5000/api/auth/login', {
+      const response = await fetch(`${config.API_URL}/api/auth/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
